4ce25246ff5d3f7a43242acef6dd27851f66ecf1
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-07 21:59:46 +0300

    Инит игры

---
+++ HomeWorkLesson3/WindowsApp3Asteroids/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp3Asteroids
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Form form = new Form
+            {
+                Width = 1024,
+                Height = 768,
+                Text = "Geekbrains C# Уровень 2. Методичка 2. Задачи № 1 - 5. Доработка игры \"Астероиды\"",
+                StartPosition = FormStartPosition.CenterScreen,
+            };
+
+            form.Show();
+
+            Application.Run(form);
+        }
+    }
+}
