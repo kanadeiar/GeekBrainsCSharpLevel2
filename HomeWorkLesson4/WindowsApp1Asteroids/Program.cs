6e998be9792014382da4dad6ce1927d727970936
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-10 22:58:32 +0300

    Инит первого задания

---
+++ HomeWorkLesson4/WindowsApp1Asteroids/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp1Asteroids
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача № 1.
+        /// 1. Добавить в программу коллекцию астероидов. Как только она
+        /// заканчивается (все астероиды сбиты), формируется новая коллекция,
+        /// в которой на один астероид больше.
+        /// Рассахатский Андрей
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Form form = new Form
+            {
+                Width = 1024,
+                Height = 768,
+                Text = "Методичка 4. Продвинутый курс C#. Задача № 1. Доработка игры \"Астероиды\"",
+                StartPosition = FormStartPosition.CenterScreen,
+            };
+
+
+            Application.Run(form);
+        }
+    }
+}
