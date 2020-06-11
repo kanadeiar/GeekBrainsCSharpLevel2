b1d30fcd3e7f9b5eecbceb8bc3b9e7d8b0791842
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-11 22:19:56 +0300

    Добавлен базовый объект

---
+++ HomeWorkLesson4/WindowsApp1Asteroids/Objects/ICollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace WindowsApp1Asteroids.Objects
+{
+    /// <summary> Столкновение </summary>
+    interface ICollision
+    {
+        /// <summary> Границы </summary>
+        Rectangle Rect { get; }
+        /// <summary> Столкновение? </summary>
+        /// <param name="obj">Другой</param>
+        /// <returns>Столкновение!</returns>
+        bool Collision(ICollision obj);
+    }
+}
