57a7ee8019288a284e2ff16512108e702a2d16b7
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-12 21:51:47 +0300

    Инит задачи 3

---
+++ HomeWorkLesson4/WindowsFormsApp3Dictionary/FormMain.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApp3Dictionary
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.BackColor = System.Drawing.Color.LightGreen;
+            this.labelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHeader.Location = new System.Drawing.Point(0, 0);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(630, 82);
+            this.labelHeader.TabIndex = 3;
+            this.labelHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 491);
+            this.Controls.Add(this.labelHeader);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Методичка 4. Продвинутый курс C#. Задача № 3.  Работа со словарем.";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHeader;
+    }
+}
+
---
+++ HomeWorkLesson4/WindowsFormsApp3Dictionary/FormMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3Dictionary
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// Задача 3
+        /// * Дан фрагмент программы:
+        /// Dictionary<string, int> dict = new Dictionary<string, int>()
+        /// {
+        ///     {"four",4 },
+        ///     {"two",2 },
+        ///     { "one",1 },
+        ///     {"three",3 },
+        /// };
+        /// var d = dict.OrderBy(delegate(KeyValuePair<string,int> pair) { return pair.Value; });
+        /// foreach (var pair in d)
+        /// {
+        ///     Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+        /// }
+        /// а. Свернуть обращение к OrderBy с использованием лямбда-выражения =>.
+        /// b. * Развернуть обращение к OrderBy с использованием делегата .
+        /// Рассахатский
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+            labelHeader.Text = "Методичка 4. Продвинутый курс C#. Объектно-ориентированное программирование. Часть 4.\n" +
+                               "Задача 3. Работа со словарем.";
+        }
+    }
+}
