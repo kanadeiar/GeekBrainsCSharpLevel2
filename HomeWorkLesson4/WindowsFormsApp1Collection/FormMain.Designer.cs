397b1aebce20df836de13336c64a3ec14022404b
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-12 17:54:58 +0300

    Завершение задания 2а

---
+++ HomeWorkLesson4/WindowsFormsApp1Collection/FormMain.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp1Collection
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonCalcNumbers = new System.Windows.Forms.Button();
+            this.textBoxResultNumbers = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.BackColor = System.Drawing.Color.LightGreen;
+            this.labelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHeader.Location = new System.Drawing.Point(0, 0);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(625, 93);
+            this.labelHeader.TabIndex = 2;
+            this.labelHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonCalcNumbers);
+            this.groupBox1.Controls.Add(this.textBoxResultNumbers);
+            this.groupBox1.Location = new System.Drawing.Point(4, 96);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(609, 172);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Подсчет элементов для коллекции целых чисел";
+            // 
+            // buttonCalcNumbers
+            // 
+            this.buttonCalcNumbers.Location = new System.Drawing.Point(8, 19);
+            this.buttonCalcNumbers.Name = "buttonCalcNumbers";
+            this.buttonCalcNumbers.Size = new System.Drawing.Size(129, 55);
+            this.buttonCalcNumbers.TabIndex = 5;
+            this.buttonCalcNumbers.Text = "Сосчитать!";
+            this.buttonCalcNumbers.UseVisualStyleBackColor = true;
+            this.buttonCalcNumbers.Click += new System.EventHandler(this.buttonCalcNumbers_Click);
+            // 
+            // textBoxResultNumbers
+            // 
+            this.textBoxResultNumbers.Location = new System.Drawing.Point(143, 19);
+            this.textBoxResultNumbers.Multiline = true;
+            this.textBoxResultNumbers.Name = "textBoxResultNumbers";
+            this.textBoxResultNumbers.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResultNumbers.Size = new System.Drawing.Size(460, 145);
+            this.textBoxResultNumbers.TabIndex = 4;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 604);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelHeader);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Методичка 4. Продвинутый курс C#. Задача № 2.  Количество элементов в коллекции.";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHeader;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxResultNumbers;
+        private System.Windows.Forms.Button buttonCalcNumbers;
+    }
+}
+
---
+++ HomeWorkLesson4/WindowsFormsApp1Collection/FormMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1Collection
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// Задача 2
+        /// Дана коллекция List<T>. Требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции:
+        /// - для целых чисел;
+        /// - * для обобщенной коллекции;
+        /// - ** используя Linq.
+        /// Рассахатский
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+            labelHeader.Text = "Методичка 4. Продвинутый курс C#. Объектно-ориентированное программирование. Часть 4.\n" +
+                "Задача 2. Требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции.";
+        }
+        
+        private void buttonCalcNumbers_Click(object sender, EventArgs e)
+        {
+            List<int> list = new List<int>
+            {
+                1,1,1,2,2,3,4,4,4,4,5,5,6,6,6,7,8,8,8,8,9,9,9,10,10,
+            };
+            var dict = GetDigitsCounts(list);
+            OutDictionaryToTextBox(dict, textBoxResultNumbers);
+        }
+
+
+        /// <summary> Подсчет количества чисел в коллекции </summary>
+        /// <param name="list">коллекция</param>
+        /// <returns>кол-во элементов</returns>
+        static Dictionary<int, int> GetDigitsCounts(List<int> list)
+        {
+            var dct = new Dictionary<int, int>();
+            foreach (var l in list)
+                if (dct.ContainsKey(l))
+                    dct[l]++;
+                else
+                    dct.Add(l, 1);
+            return dct;
+        }
+
+        /// <summary> Вывод информации из словаря в текстовое поле на форме </summary>
+        /// <param name="dict">словарь</param>
+        /// <param name="textBox">текстовое поле</param>
+        static void OutDictionaryToTextBox(Dictionary<int, int> dict, TextBox textBox)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var d in dict)
+                sb.AppendLine($"Число {d.Key} встречается в массиве {d.Value} раз.");
+            textBox.Text = sb.ToString();
+        }
+    }
+}
