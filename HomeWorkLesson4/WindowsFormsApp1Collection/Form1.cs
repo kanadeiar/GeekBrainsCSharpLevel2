6a59eaa61614471f2876e23e57481f18cad100a3
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-12 09:50:11 +0300

    Инит задачи №2

---
+++ HomeWorkLesson4/WindowsFormsApp1Collection/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1Collection
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Задача 2
+        /// Дана коллекция List<T>. Требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции:
+        /// - для целых чисел;
+        /// - * для обобщенной коллекции;
+        /// - ** используя Linq.
+        /// Рассахатский
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
