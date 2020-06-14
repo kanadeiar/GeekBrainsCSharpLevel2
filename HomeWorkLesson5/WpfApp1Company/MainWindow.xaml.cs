48e35d2e8981ab0aade3c1b08bd26f1c50617ec5
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-14 23:49:31 +0300

    Инит приложения

---
+++ HomeWorkLesson5/WpfApp1Company/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1Company
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Задачи 1-4.
+        /// Создать WPF-приложение для ведения списка сотрудников компании.
+        /// Создать сущности Employee и Department и заполнить списки
+        /// сущностей начальными данными.
+        /// Для списка сотрудников и списка департаментов предусмотреть
+        /// визуализацию (отображение). Это можно сделать, например, с
+        /// использованием ComboBox или ListView.
+        /// Предусмотреть редактирование сотрудников и департаментов.
+        /// Должна быть возможность изменить департамент у сотрудника.
+        /// Список департаментов для выбора можно выводить в ComboBox, и
+        /// все это можно выводить на дополнительной форме.
+        /// Предусмотреть возможность создания новых сотрудников и
+        /// департаментов.
+        /// Реализовать это либо на форме редактирования, либо сделать
+        /// новую форму.
+        /// Рассахатский
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
