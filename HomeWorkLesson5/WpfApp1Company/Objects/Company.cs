1bbee2e65486379caa205773481be8f4d8ae10fa
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-16 05:00:01 +0300

    Добавлены классы

---
+++ HomeWorkLesson5/WpfApp1Company/Objects/Company.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1Company.Objects
+{
+    /// <summary>
+    /// Компания
+    /// </summary>
+    class Company
+    {
+        /// <summary> Название </summary>
+        public string Name { get; set; }
+        public List<Departament> Departaments { get; set; }
+        public List<Employee> Employees { get; set; }
+        public static (List<Departament>, List<Employee>) GetSamples()
+        {
+            List<Departament> listD = new List<Departament>
+            {
+                new Departament {Id = 1, Name = "Отдел финансов"},
+                new Departament {Id = 2, Name = "Отдел маркетинга"},
+                new Departament {Id = 3, Name = "Отдел рекламы"},
+            };
+            List<Employee> listE = new List<Employee>
+            {
+                new Employee {Fam = "Иванов", Name = "Иван", Age = 18, Salary = 12000, IdDepartament = 1},
+                new Employee {Fam = "Федотов", Name = "Федот", Age = 18, Salary = 12000, IdDepartament = 1},
+                new Employee {Fam = "Петров", Name = "Петя", Age = 18, Salary = 12000, IdDepartament = 1},
+                new Employee {Fam = "Агафонов", Name = "Михаил", Age = 18, Salary = 12000, IdDepartament = 1},
+                new Employee {Fam = "Яварова", Name = "Яна", Age = 18, Salary = 12000, IdDepartament = 1},
+                new Employee {Fam = "Агафонова", Name = "Аня", Age = 18, Salary = 12000, IdDepartament = 1},
+            };
+            return (listD, listE);
+        }
+        public Company(string name)
+        {
+            Name = name;
+            Departaments = new List<Departament>();
+            Employees = new List<Employee>();
+        }
+
+
+    }
+}
---
+++ HomeWorkLesson5/WpfApp1Company/Objects/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1Company.Objects
+{
+    /// <summary> Сотрудник компании </summary>
+    class Employee
+    {
+        /// <summary> Фамилия </summary>
+        public string Fam { get; set; }
+        /// <summary> Имя </summary>
+        public string Name { get; set; }
+        /// <summary> Возраст </summary>
+        public int Age { get; set; }
+        /// <summary> Зарплата </summary>
+        public double Salary { get; set; }
+        /// <summary> Идентификатор департамента </summary>
+        public int IdDepartament { get; set; }
+    }
+}
