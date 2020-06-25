a8d0c5e301e3601f48dd2f063e787ed5377c151e
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-25 22:00:23 +0300

    Добавлен контроллер, модель отделов + источник бд

---
+++ HomeWorkLesson8/WebApplication1/Controllers/DepartmentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public IEnumerable<Department> GetAllDepartments() => Department.Departments;
+        public IHttpActionResult GetDepartment(int id)
+        {
+            var dep = Department.OneDepartment(id);
+            if (dep == null)
+                return NotFound();
+            return Ok(dep);
+        }
+    }
+}
---
+++ HomeWorkLesson8/WebApplication1/Models/Department.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    /// <summary> Отдел </summary>
+    public class Department
+    {
+        /// <summary> Идентификатор </summary>
+        public int Id { get; set; }
+        /// <summary> Название </summary>
+        public string Dep { get; set; }
+        private static SqlDataAdapter _adapter = new SqlDataAdapter();
+        private static DataTable _table = new DataTable();
+        static Department()
+        {
+            SqlCommand command = new SqlCommand("SELECT id,department FROM Department;", 
+                DataBaseSource.Connection);
+            _adapter.SelectCommand = command;
+        }
+        /// <summary> Все отделы </summary>
+        public static IList<Department> Departments => GetTable();
+        /// <summary> Один отдел </summary>
+        /// <param name="id">идентификатор</param>
+        /// <returns>отдел</returns>
+        public static Department OneDepartment(int id)
+        {
+            return GetTable().FirstOrDefault(d => d.Id == id);
+        }
+        private static IList<Department> GetTable()
+        {
+            _table.Clear();
+            _adapter.Fill(_table);
+            IList<Department> departments = new List<Department>();
+            foreach (DataRow el in _table.Rows)
+            {
+                departments.Add(new Department
+                {
+                    Id = Convert.ToInt32(el.ItemArray[0]),
+                    Dep = el.ItemArray[1].ToString(),
+                });
+            }
+            return departments;
+        }
+    }
+}
